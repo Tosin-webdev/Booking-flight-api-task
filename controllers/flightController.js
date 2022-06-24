f4169a05c8f0132f22c2ad56522adb6c75f4539e
tosin-webdev <oladejit5@gmail.com>
2022-06-24 12:59:15 +0100

    updated file

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -10,7 +10,7 @@ exports.get = (req, res) => {
   console.log(req.params.id);
   const flight = flights.find((c) => c.id === parseInt(req.params.id));
   if (!flight) {
-    return res.status(400).send('The flight with the given ID was not found');
+    return res.status(404).send('The flight with the given ID was not found');
   }
   res.send(flight);
 };
@@ -35,7 +35,7 @@ exports.create = (req, res) => {
 exports.update = (req, res) => {
   const flight = flights.find((c) => c.id === parseInt(req.params.id));
   if (!flight) {
-    return res.status(400).send('The flight with the given ID was not found');
+    return res.status(404).send('The flight with the given ID was not found');
   }
   flight.title = req.body.title;
   flight.time = new Date().toLocaleTimeString();
